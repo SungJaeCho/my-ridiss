77e9ab4c74b881c7f01f1fd75fb7d12c53fddfb6
SungJaeCho <liadus@naver.com>
2018-07-16 00:10:37 +0900

    initial commit

---
+++ src/main/java/com/ridiss/MyRidissApplication.java
@@ -0,0 +1,12 @@
+package com.ridiss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRidissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRidissApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ridiss/MyRidissApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ridiss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyRidissApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
