244a5043147ff8b5afc1e06ea1557c53875a259b
SungJaeCho <liadus@naver.com>
2018-07-20 00:02:07 +0900

    박재상 2-2

---
+++ src/main/java/com/ridiss/web/UserController.java
@@ -0,0 +1,18 @@
+package com.ridiss.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+	
+	@PostMapping("/create")
+	public String create(User user) {
+		
+		System.out.println("user : " + user);
+		return "index";
+	}
+	
+	
+	
+}
